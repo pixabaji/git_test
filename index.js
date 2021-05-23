e47387640a2090b5a76207801dc80d6bfb944000
ivor pixabaj <pixabaji@gmail.com>
2021-05-23 15:49:00 -0600

    This should be prod version

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('dfsdfsd');
+const config = require('./ds-config.json');
+class TestGit {
+
+    constructor() {
+        console.log("Initiating test git...");
+    }
+
+    showConfigData() {
+        console.log("SHOWING CONFIF DATA _____________________________________________________________\n");
+        console.log(config.gitStatus);
+        console.log(config.name);
+        console.log(config.num);
+        console.log(config.port);
+    }
+
+}
+
+let testGit = new TestGit();
+testGit.showConfigData();
