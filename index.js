b4ff97f7819e34b3b5bacd4a655ca44139441a24
ivor pixabaj <pixabaji@gmail.com>
2021-05-23 15:56:51 -0600

    Prod final push

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const config = require('./ds-config.json');
+const intConfig = require('./ds-internal.json');
+const mongoConfig = require('./ds-mongo.json');
 class TestGit {
 
     constructor() {
@@ -6,11 +8,23 @@ class TestGit {
     }
 
     showConfigData() {
-        console.log("SHOWING CONFIF DATA _____________________________________________________________\n");
+        console.log("SHOWING CONFIG DATA _____________________________________________________________________\n");
         console.log(config.gitStatus);
         console.log(config.name);
         console.log(config.num);
         console.log(config.port);
+
+        console.log("SHOWING INTERNAL CONFIG DATA _____________________________________________________________\n");
+        console.log(intConfig.gitStatus);
+        console.log(intConfig.name);
+        console.log(intConfig.num);
+        console.log(intConfig.port);
+
+        console.log("SHOWING MONGO CONFIG DATA ________________________________________________________________\n");
+        console.log(mongoConfig.gitStatus);
+        console.log(mongoConfig.name);
+        console.log(mongoConfig.num);
+        console.log(mongoConfig.port);
     }
 
 }
